ef44de9723b674993cefb8a4ca2d13cbe1709314
Do Manh Quang <domanhquang2006@gmail.com>
2026-04-23 19:33:33 +0700

    fix: giải quyết xung đột và cập nhật App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// App.tsx
 import { DAppKitProvider } from '@mysten/dapp-kit-react';
 import { ConnectButton } from '@mysten/dapp-kit-react/ui';
 import { dAppKit } from './dapp-kit';
@@ -16,15 +14,3 @@ export default function App() {
 		</DAppKitProvider>
 	);
 }
-=======
-function App() {
-
-  return (
-    <>
-      <h1 className="text-red-500">Hello World</h1>
-    </>
-  )
-}
-
-export default App
->>>>>>> 084f0bc076a555d84f326ed30de4f6cdc6825f85
